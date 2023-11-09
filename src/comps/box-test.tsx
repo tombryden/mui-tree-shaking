d068446eba279ac9bacaf90b6d8ed4ff81a9de8a
Tom Bryden <tombryden@me.com>
2023-11-09 06:49:04 +0000

    Init

---
+++ src/comps/box-test.tsx
@@ -0,0 +1,11 @@
+// "use client";
+
+import { Box, Typography } from "@mui/material";
+
+export default function name() {
+  return (
+    <Box>
+      <Typography>Test</Typography>
+    </Box>
+  );
+}
